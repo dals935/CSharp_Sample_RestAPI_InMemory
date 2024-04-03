ea2146355bb292d039e4dff315ef3d31dea677ff
John Ryan Andal <124016396+dals935@users.noreply.github.com>
2024-04-03 13:41:21 +0800

    Add files via upload

---
+++ WebApiDemo/WebApiDemo/Filters/ActionFilters/Shirt_ValidateCreateShirtFilterAttribute.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using WebApiDemo.Models.Repositories;
+using WebApiDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDemo.Filters.ActionFilters
+{
+    public class Shirt_ValidateCreateShirtFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            var shirt = context.ActionArguments["shirt"] as Shirt;
+
+            if (shirt == null)
+            {
+                context.ModelState.AddModelError("Shirt", "Shirt Object Is Null");
+                var problemDetails = new ValidationProblemDetails(context.ModelState)
+                {
+                    Status = StatusCodes.Status400BadRequest
+                };
+                context.Result = new BadRequestObjectResult(problemDetails);
+            }
+            else
+            {
+                var existingShirt = ShirtRepository.GetShirtByProperties(shirt.Brand, shirt.Gender, shirt.Color, shirt.Size);
+                if (existingShirt != null)
+                {
+                    context.ModelState.AddModelError("Shirt", "Shirt Already Exists");
+                    var problemDetails = new ValidationProblemDetails(context.ModelState)
+                    {
+                        Status = StatusCodes.Status400BadRequest
+                    };
+                    context.Result = new BadRequestObjectResult(problemDetails);
+                }
+            }
+
+        }
+    }
+}
---
+++ WebApiDemo/WebApiDemo/Models/Repositories/ShirtRepository.cs
@@ -0,0 +1,71 @@
+﻿namespace WebApiDemo.Models.Repositories
+{
+    public static class ShirtRepository
+    {
+        private static List<Shirt> shirts = new List<Shirt>()
+        {
+            new Shirt { ShirtId = 1, Brand = "MyBrand", Color = "Blue", Gender = "Men", Price = 69, Size = 10},
+            new Shirt { ShirtId = 2, Brand = "YourBrand", Color = "Red", Gender = "Men", Price = 420, Size = 12},
+            new Shirt { ShirtId = 3, Brand = "My_Brand", Color = "Green", Gender = "Women", Price = 13, Size = 8},
+            new Shirt { ShirtId = 4, Brand = "Your_Brand", Color = "Yellow", Gender = "Women", Price = 70, Size = 9},
+        };
+
+        public static List<Shirt> GetShirts()
+        {
+            return shirts;
+        }
+
+        public static bool ShirtExists(int id)
+        {
+            return shirts.Any(x => x.ShirtId == id);
+        }
+
+        public static Shirt? GetShirtById(int id)
+        {
+            return shirts.FirstOrDefault(x =>x.ShirtId == id);
+        }
+
+        public static Shirt? GetShirtByProperties(string? brand, string? gender, string? color, int? size)
+        {
+            return shirts.FirstOrDefault(x =>
+                !string.IsNullOrWhiteSpace(brand) &&
+                !string.IsNullOrWhiteSpace(x.Brand) &&
+                x.Brand.Equals(brand, StringComparison.OrdinalIgnoreCase) &&
+                !string.IsNullOrWhiteSpace(gender) &&
+                !string.IsNullOrWhiteSpace(x.Gender) &&
+                x.Gender.Equals(gender, StringComparison.OrdinalIgnoreCase) &&
+                !string.IsNullOrWhiteSpace(color) &&
+                !string.IsNullOrWhiteSpace(x.Color) &&
+                x.Color.Equals(color, StringComparison.OrdinalIgnoreCase) &&
+                size.HasValue &&
+                x.Size.HasValue &&
+                size.Value == x.Size.Value);
+        }
+
+        public static void AddShirt(Shirt shirt)
+        {
+            int maxId = shirts.Max(x => x.ShirtId);
+            shirt.ShirtId = maxId + 1;
+            shirts.Add(shirt);
+        }
+
+        public static void UpdateShirt(Shirt shirt)
+        {
+            var shirtToUpdate = shirts.First(x => x.ShirtId == shirt.ShirtId);
+            shirtToUpdate.Brand = shirt.Brand;
+            shirtToUpdate.Price = shirt.Price;
+            shirtToUpdate.Size = shirt.Size;
+            shirtToUpdate.Color = shirt.Color;
+            shirtToUpdate.Gender = shirt.Gender;
+        }
+
+        public static void DeleteShirt(int shirtId)
+        {
+            var shirt = GetShirtById(shirtId); 
+            if (shirt != null)
+            {
+                shirts.Remove(shirt);
+            }
+        }
+    }
+}
